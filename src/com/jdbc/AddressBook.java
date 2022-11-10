a4cd48f8cd6ef83c7795492d18db7833a7899009
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-11-10 10:39:33 +0530

    [Added: retreve data between range]

--- src/com/jdbc/AddressBook.java
+++ src/com/jdbc/AddressBook.java
@@ -26,6 +26,7 @@ public class AddressBook {
 		return connection;
 	}
 
+
 	public List<Contacts> retrieveData() {
 		ResultSet resultSet = null;
 		List<Contacts> addressBookList = new ArrayList<Contacts>();
@@ -53,5 +54,79 @@ public class AddressBook {
 		}
 		return addressBookList;
 
+	}
+
+	public void updateCityByZip(String address, String city, String state, int zip, int id) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String query = "Update addressbook_table set address=" + "'" + address + "'" + ", " + "city=" + "'" + city
+					+ "'" + ", " + "state=" + "'" + state + "'" + ", " + "zipcode=" + zip + " where id=" + id + ";";
+			int result = statement.executeUpdate(query);
+			System.out.println(result);
+			if (result > 0) {
+				System.out.println("Address Updated Successfully");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Contacts> findAllForParticularDate(LocalDate date) {
+		ResultSet resultSet = null;
+		List<Contacts> addressBookList = new ArrayList<Contacts>();
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select * from addressbook_table where dateadded between cast(' " + date + "'"
+					+ " as date)  and date(now());";
+			resultSet = statement.executeQuery(sql);
+			int count = 0;
+			while (resultSet.next()) {
+				Contacts contactInfo = new Contacts();
+				contactInfo.setFirstName(resultSet.getString("firstName"));
+				contactInfo.setLastName(resultSet.getString("lastname"));
+				contactInfo.setAddress(resultSet.getString("address"));
+				contactInfo.setCity(resultSet.getString("city"));
+				contactInfo.setState(resultSet.getString("state"));
+				contactInfo.setZip(resultSet.getInt("zip"));
+				contactInfo.setPhoneNumber(resultSet.getString("PhoneNumber"));
+				contactInfo.setEmailId(resultSet.getString("email"));
+				contactInfo.setBookName(resultSet.getString("booknamed"));
+				contactInfo.setDateAdded(resultSet.getDate("dateadded").toLocalDate());
+
+				addressBookList.add(contactInfo);
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return addressBookList;
+	}
+
+	public int countByCity(String city) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select count(firstname) from addressbook_table where city=" + "'" + city + "';";
+			ResultSet result = statement.executeQuery(sql);
+			result.next();
+			int count = result.getInt(1);
+
+			return count;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public int countByState(String state) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select count(firstname) from addressbook_table where city=" + "'" + state + "';";
+			ResultSet result = statement.executeQuery(sql);
+			result.next();
+			int count = result.getInt(1);
+			return count;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
--- src/com/jdbc/AddressBookMain.java
+++ src/com/jdbc/AddressBookMain.java
@@ -1,35 +1,86 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookMain {
 
-	 static Scanner scanner = new Scanner(System.in);
+	static Scanner scanner = new Scanner(System.in);
 
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
+		boolean exit = false;
+		while (!exit) {
+			System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Update Address,city,state,zip  by srNo\n "
+					+ "3 -> Retrieve data for particular date\n" + "4 -> Retrieve Count of Contacts for City or State\n"
+					+ "5 -> exit");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				retrieveData();
+				break;
+			case 2:
+				update();
+				break;
+			case 3:
+				reteriveDataForParticularDate();
+				break;
+			case 4:
+				retrieveCountByCityOrState();
+			case 5:
+				exit = true;
+			}
+		}
+	}
 
-	        boolean exit = false;
-	        while (!exit) {
-	            System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Exit");
-	            switch (scanner.nextInt()) {
-	                case 1:
-	                    retrieveData();
-	                    break;
-	                case 2:
-	                    exit = true;
-	            }
-	        }
-	    }
+	private static void retrieveCountByCityOrState() {
+		AddressBook addressBookRepo = new AddressBook();
+		System.out.println("Enter 1 -> Contacts count by City\n" + "2 -> Contacts count by State");
 
-	    private static void retrieveData() {
-	        AddressBook addressBookRepo = new AddressBook();
-	        List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
-	        for (Contacts employee : employeeInfoList)
-	         {
-	            System.out.println(employee + "\n");
-	        }
-	    }
+		switch (scanner.nextInt()) {
+		case 1:
+			System.out.println("Enter city Name");
+			int cityContactsCount = addressBookRepo.countByCity(scanner.next());
+			System.out.println("Number of Contacts is Given city= " + cityContactsCount);
+			break;
+		case 2:
+			System.out.println("Enter state name");
+			int stateContactsCount = addressBookRepo.countByState(scanner.next());
+			System.out.println("Number of Contacts is Given state= " + stateContactsCount);
+			break;
+		}
+
+	}
+
+	private static void retrieveData() {
+		AddressBook addressBookRepo = new AddressBook();
+		List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
+		for (Contacts employee : employeeInfoList) {
+			System.out.println(employee + "\n");
+		}
+	}
+
+	private static void update() {
+		AddressBook addressBookRepo = new AddressBook();
+		System.out.println("Enter the address,city,state, zip and id  to Update");
+		addressBookRepo.updateCityByZip(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(),
+				scanner.nextInt());
+	}
+
+	private static void reteriveDataForParticularDate() {
+		AddressBook addressBook = new AddressBook();
+		System.out.println("Enter the Date of Joining (YYYY-MM-DD");
+		System.out.println("Enter year , month and Day ex: 2020 02 03");
+		List<Contacts> employeeInfoList = addressBook
+				.findAllForParticularDate(LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()));
+		for (Contacts employee : employeeInfoList) {
+			System.out.println(employee + "\n");
+		}
+	}
 }
